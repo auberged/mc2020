cc771857f6703d9d8e55185b21f6f43d91ac4cd9
Stefan <stefan.erben1@gmail.com>
2020-06-02 00:23:47 +0200

    add MovieModel prototyp

---
+++ MC2020App/app/src/main/java/at/technikumwien/mc2020/utilities/MovieModel.java
@@ -0,0 +1,13 @@
+package at.technikumwien.mc2020.utilities;
+
+import java.util.List;
+
+public class MovieModel {
+    private int id;
+    private String title;
+    private String description;
+    private double vote_average;
+    private String poster_url;
+    private String releaseDate;
+    private List<String> genres;
+}
--- MC2020App/app/src/test/java/at/technikumwien/mc2020/ExampleUnitTest.java
+++ MC2020App/app/src/test/java/at/technikumwien/mc2020/ExampleUnitTest.java
@@ -1,10 +1,16 @@
 package at.technikumwien.mc2020;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.junit.Test;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.LinkedList;
+import java.util.List;
 
+import at.technikumwien.mc2020.utilities.MovieModel;
 import at.technikumwien.mc2020.utilities.NetworkUtils;
 
 /**
@@ -18,24 +24,64 @@ public class ExampleUnitTest {
 
     @Test
     public void addition_isCorrect() {
-        System.out.println("Test");
+
+        // image: https://image.tmdb.org/t/p/w600_and_h900_bestv2/dCdTAOxkcNnsFVKHQnxbklvGRzF.jpg
 
         URL apiUrl = NetworkUtils.buildUrl("https://api.themoviedb.org/3/discover/movie?api_key=db94b1f559af23f5a8bd53a8dbec0c1e&language=de&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&year=2019", 5);
         System.out.println(apiUrl.toString());
 
-        // image: https://image.tmdb.org/t/p/w600_and_h900_bestv2/dCdTAOxkcNnsFVKHQnxbklvGRzF.jpg
-
+        String apiResult = null;
         try {
-            String apiResult = NetworkUtils.getResponseFromHttpUrl(apiUrl);
+            apiResult = NetworkUtils.getResponseFromHttpUrl(apiUrl);
             System.out.println(apiResult);
         } catch (IOException e) {
             e.printStackTrace();
             return;
         }
 
+        //parseDataToMovies(apiResult);
+
 
 
         //assertEquals(4, 2 + 2);
+    }
+
+    public List<MovieModel> parseDataToMovies(String apiResult) throws JSONException {
+        if(apiResult == null)
+            return null;
+
+        // Parse to object
+        JSONObject jsonObject = new JSONObject(apiResult);
+
+        // Parse to array of cards
+        JSONArray cardsArray = jsonObject.getJSONArray("results");
+
+        // If page is empty, send new request to first page
+        if(cardsArray.length() == 0){
+            // TODO add correct end condition (e.g. page = page in response)
+            return null;
+        }
+
+        // Make a new empty list
+        List<MovieModel> movies = new LinkedList<MovieModel>();
+
+        // Generate each card and add it to the list
+        for (int i = 0; i < cardsArray.length(); i++) {
+            JSONObject jsonCard = cardsArray.getJSONObject(i);
+            String name = jsonCard.getString("name");
+
+            MovieModel movie = new MovieModel(name, type, rarity);
+
+            JSONArray colors = jsonCard.getJSONArray("colors");
+            for (int j = 0; j < colors.length(); j++) {
+                movie.addColor(colors.getString(j));
+            }
+
+            movies.add(movie);
+        }
+
+        return movies;
+
     }
 
 
