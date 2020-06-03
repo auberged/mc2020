0eca5d6e1a21f347db24da02501fa3ea2c2117b9
auda <wi18b015@technikum-wien.at>
2020-06-03 19:01:02 +0200

    Added Firebase Authentication component.

--- MC2020App/app/src/main/java/at/technikumwien/mc2020/ui/main/MainActivity.java
+++ MC2020App/app/src/main/java/at/technikumwien/mc2020/ui/main/MainActivity.java
@@ -1,20 +1,53 @@
 package at.technikumwien.mc2020.ui.main;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ImageButton;
+import android.widget.Button;
+
+import com.firebase.ui.auth.AuthUI;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
 import at.technikumwien.mc2020.R;
-import at.technikumwien.mc2020.ui.settings.SettingsActivity;
+import at.technikumwien.mc2020.ui.launcher.LauncherActivity;
 
 public class MainActivity extends AppCompatActivity {
+    private Button logoutButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        logoutButton = (Button)findViewById(R.id.logout_button);
+
+        logoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                logout();
+            }
+        });
+
+        // Write a message to the database
+        /*FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+
+        myRef.setValue("Hello, World!");*/
     }
+
+    private void logout(){
+        AuthUI.getInstance()
+                .signOut(this)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    public void onComplete(@NonNull Task<Void> task) {
+                        Intent intent = new Intent(MainActivity.this, LauncherActivity.class);
+                        startActivity(intent);
+                    }
+                });
+    }
+
 }
