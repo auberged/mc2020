fcbca711e9aa61f06726a28903dc15fd0fa615d4
auda <wi18b015@technikum-wien.at>
2020-06-13 00:03:39 +0200

    Implemented InfinitePlaceholderView. Added Paging to load more movies if user scrolls down. Added swipe function to delete movie if user swipes to the left.

--- MC2020App/app/src/main/java/at/technikumwien/mc2020/MovieCard.java
+++ MC2020App/app/src/main/java/at/technikumwien/mc2020/MovieCard.java
@@ -6,7 +6,9 @@ import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
 import com.mindorks.placeholderview.SwipePlaceHolderView;
+import com.mindorks.placeholderview.annotations.Animate;
 import com.mindorks.placeholderview.annotations.Layout;
+import com.mindorks.placeholderview.annotations.NonReusable;
 import com.mindorks.placeholderview.annotations.Resolve;
 import com.mindorks.placeholderview.annotations.View;
 import com.mindorks.placeholderview.annotations.swipe.SwipeCancelState;
@@ -15,6 +17,7 @@ import com.mindorks.placeholderview.annotations.swipe.SwipeInState;
 import com.mindorks.placeholderview.annotations.swipe.SwipeOut;
 import com.mindorks.placeholderview.annotations.swipe.SwipeOutState;
 
+import at.technikumwien.mc2020.data.database.FirebaseHandler;
 import at.technikumwien.mc2020.utilities.MovieModel;
 
 
@@ -22,13 +25,15 @@ import at.technikumwien.mc2020.utilities.MovieModel;
  * Inspired by https://blog.mindorks.com/android-tinder-swipe-view-example-3eca9b0d4794
  * See step 9
  */
+@NonReusable
+@Animate(Animate.CARD_BOTTOM_IN_ASC)
 @Layout(R.layout.movie_card_layout)
 public class MovieCard {
 
     @View(R.id.iv_movie_poster_swipe)
-    private ImageView profileImageView;
+    public ImageView profileImageView;
 
-    private String ImageUrl; // Replace Image with the Movie Class later....
+    private String ImageUrl;
     private Context mContext;
     private SwipePlaceHolderView mSwipeView;
     private MovieModel movieData;
@@ -39,44 +44,49 @@ public class MovieCard {
         this.mContext = mContext;
         this.mSwipeView = mSwipeView;
 
-
     }
 
-    private void awesomeButtonClicked() {
+    public MovieModel getMovieData() {
+        return movieData;
+    }
+
+    public void awesomeButtonClicked() {
         Log.d("TINDER", "more details");
     }
 
-
     @Resolve
-    private void onResolved(){
+    public void onResolved() {
         // Load here the Movie Poster into the layout
         Glide.with(mContext).load(ImageUrl).into(profileImageView);
     }
 
     @SwipeOut
-    private void onSwipedOut(){
-        Log.d("TINDER", "Disliked movie: " + movieData.title +  " " + movieData.poster_url);
-        //mSwipeView.addView(this);
+    public void onSwipedOut() {
+        Log.d("TINDER", "Disliked movie: " + movieData.title + " " + movieData.poster_url);
+        FirebaseHandler.getInstance().saveDislikedMovie(movieData);
+        //FirebaseDatabase.getInstance().getReference().child(Constants.DISLIKED_MOVIES).child(FirebaseAuth.getInstance().getUid()).child(String.valueOf(movieData.id)).setValue(movieData.title);
     }
 
     @SwipeCancelState
-    private void onSwipeCancelState(){
+    public void onSwipeCancelState() {
         Log.d("TINDER", "onSwipeCancelState");
     }
 
     @SwipeIn
-    private void onSwipeIn(){
+    public void onSwipeIn() {
 
-        Log.d("TINDER", "Liked movie: " + movieData.title +  " " + movieData.poster_url);
+        Log.d("TINDER", "Liked movie: " + movieData.title + " " + movieData.poster_url);
+        FirebaseHandler.getInstance().saveLikedMovie(movieData);
+
     }
 
     @SwipeInState
-    private void onSwipeInState(){
+    public void onSwipeInState() {
         Log.d("TINDER", "onSwipeInState");
     }
 
     @SwipeOutState
-    private void onSwipeOutState(){
+    public void onSwipeOutState() {
         Log.d("TINDER", "onSwipeOutState");
     }
 }
---
+++ MC2020App/app/src/main/java/at/technikumwien/mc2020/ui/list/MovieItem.java
@@ -0,0 +1,87 @@
+package at.technikumwien.mc2020.ui.list;
+
+import android.content.Context;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.cardview.widget.CardView;
+
+import com.bumptech.glide.Glide;
+import com.mindorks.placeholderview.annotations.Animate;
+import com.mindorks.placeholderview.annotations.Click;
+import com.mindorks.placeholderview.annotations.Layout;
+import com.mindorks.placeholderview.annotations.LongClick;
+import com.mindorks.placeholderview.annotations.NonReusable;
+import com.mindorks.placeholderview.annotations.Position;
+import com.mindorks.placeholderview.annotations.Recycle;
+import com.mindorks.placeholderview.annotations.Resolve;
+import com.mindorks.placeholderview.annotations.View;
+
+import at.technikumwien.mc2020.R;
+import at.technikumwien.mc2020.utilities.MovieModel;
+
+@NonReusable
+@Animate(Animate.CARD_BOTTOM_IN_ASC)
+@Layout(R.layout.load_movie_item_view)
+public class MovieItem {
+    @View(R.id.card_view)
+    CardView cardView;
+
+    @View(R.id.image_movie_view)
+    ImageView imageView;
+
+    @View(R.id.movie_title)
+    TextView movieTitle;
+
+    @Position
+    int position;
+
+    private MovieModel movieModel;
+    private Context context;
+
+    public MovieItem(Context context, MovieModel movieModel) {
+        this.context = context;
+        this.movieModel = movieModel;
+    }
+
+    /*
+     * This method is called when the view is rendered
+     * onResolved method could be named anything, Example: onAttach
+     */
+    @Resolve
+    public void onResolved() {
+        // do something here
+        // example: load imageView with url image
+        Glide.with(context).load(movieModel.poster_url).into(imageView);
+        movieTitle.setText(movieModel.title);
+    }
+
+    /*
+     * This method is called when the view holder is recycled
+     * and used to display view for the next data set
+     */
+    @Recycle
+    public void onRecycled() {
+        // do something here
+        // Example: clear some references used by earlier rendering
+    }
+
+    /*
+     * This method is called when the view with id image_view is clicked.
+     * onImageViewClick method could be named anything.
+     */
+    @Click(R.id.image_movie_view)
+    public void onImageViewClick() {
+        // do something
+
+    }
+
+    @LongClick(R.id.image_movie_view)
+    public void onImageViewLongClick() {
+        // do something
+    }
+
+    public MovieModel getMovieModel(){
+        return this.movieModel;
+    }
+}
